d1fd2da61f2f2b699ac52de31827706a82c8681f
Yin Bohan <kof3d@qq.com>
2017-09-09 22:18:04 +0800

    W & H

---
+++ form/form validation(register JS )/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var aInput = document.getElementsByTagName("input");
+	var oName = aInput[0];
+	var pwd = aInput[1];
+	var pwd2 = aInput[2];
+	var aP = document.getElementsByName('p');
+}
